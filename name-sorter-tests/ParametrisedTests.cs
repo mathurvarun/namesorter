e7e51f160741ffdf44f9f9d2000c2254e9cc4123
Varun Mathur <mathurvarun@hotmail.com>
2023-11-29 10:05:15 +1000

    Update ParametrisedTests.cs

--- name-sorter-tests/ParametrisedTests.cs
+++ name-sorter-tests/ParametrisedTests.cs
@@ -4,25 +4,22 @@ namespace name_sorter_tests;
 
 public class ParametrisedTests
 {
-   
-    public bool IsSortedCorrectly(Person a, Person b, Person c, Person d)
+    [Fact]
+    public void IsSortedCorrectly()
     {
         var persons = new List<Person>();
-        persons.Add(a);
-        persons.Add(b);
-        persons.Add(c);
-        persons.Add(d);
+       
         persons = Helper.GetSortedPersonList(persons);
         
         var alphabetical = !persons.Where((s, n) => n < persons.Count - 1 && StringComparer.Ordinal.Compare(persons[n].LastName, persons[n + 1].LastName) > 0).Any();
-        return alphabetical;
+        Assert.True(alphabetical);
     }
 
     [Theory]
     [MemberData(nameof(TestPersonDataGenerator.GetPersonFromDataGenerator), MemberType = typeof(TestPersonDataGenerator))]
     public void AllPersons_AreSortedCorrectly_WithPersonData_FromDataGenerator(Person a, Person b, Person c, Person d)
     {
-        Assert.True(IsSortedCorrectly(a,b,c,d));
+        
         
     }
 }
